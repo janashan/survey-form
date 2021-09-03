f66459facefc3ca0614abd2a71bc4749d9e28705
JanarthananShanmugam <janarthanan.shanmugam@gmail.com>
2021-09-03 17:06:32 +0530

    complete survey form

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('star1').addEventListener('click',function(){
+    document.getElementById('star1').style.color='gold';
+})
+
+document.getElementById('star2').addEventListener('click',function(){
+    document.getElementById('star2').style.color='gold';
+})
+
+document.getElementById('star3').addEventListener('click',function(){
+    document.getElementById('star3').style.color='gold';
+})
+
+document.getElementById('star4').addEventListener('click',function(){
+    document.getElementById('star4').style.color='gold';
+})
+
+document.getElementById('star5').addEventListener('click',function(){
+    document.getElementById('star5').style.color='gold';
+})
